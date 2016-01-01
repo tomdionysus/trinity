38108e8fb1fc5eec125f2cec03eb44f7370519c8
Tom Cully <tom.cully@apitalent.co.nz>
2016-01-01 21:25:18 +1300

    Replaced KVStore with binarytree implementation

--- kvstore/kvstore.go
+++ kvstore/kvstore.go
@@ -2,24 +2,30 @@ package kvstore
 
 import(
   "github.com/tomdionysus/trinity/util"
+  ch "github.com/tomdionysus/consistenthash"
+  bt "github.com/tomdionysus/binarytree"
   "time"
 )
 
 // In Memory Key Value Store for testing.
 type KVStore struct {
   Logger *util.Logger
-  store map[string][]byte
-  flags map[string]int16
-  expiry map[int64][]string
+  store *bt.Tree
+  expiry map[int64][]ch.Key
   running bool
+}
+
+type Item struct {
+  Key string
+  Data []byte
+  Flags int16
 }
 
 func NewKVStore(logger *util.Logger) *KVStore {
   inst := &KVStore{
     Logger: logger,
-    store: map[string][]byte{},
-    flags: map[string]int16{},
-    expiry: map[int64][]string{},
+    store: bt.NewTree(),
+    expiry: map[int64][]ch.Key{},
     running: false,
   }
   return inst
@@ -38,7 +44,7 @@ func (me *KVStore) Start() {
       toexpire, found := me.expiry[expiretime]
       if found {
         me.Logger.Debug("KVStore","Expiring Time %d", expiretime)
-        for _, k := range toexpire { me.Delete(k) }
+        for _, k := range toexpire { me.deleteKey(k) }
         delete(me.expiry, expiretime)
       }
       time.Sleep(900*time.Millisecond)
@@ -51,12 +57,14 @@ func (me *KVStore) Stop() {
 }
 
 func (me *KVStore) Set(key string, value []byte, flags int16, expiry *time.Time) {
-  me.store[key] = value
-  me.flags[key] = flags
+  keymd5 := ch.NewMD5Key(key)
+  item := &Item{ Key: key, Data: value, Flags: flags }
+  me.store.Set(keymd5, item)
+  
   if expiry!=nil {
     exptime := expiry.UTC().Unix()
     me.Logger.Debug("KVStore","SET [%s] %s - Expiry %d", key, value, exptime)
-    me.expiry[exptime] = append(me.expiry[exptime], key)
+    me.expiry[exptime] = append(me.expiry[exptime], keymd5)
   } else {
     me.Logger.Debug("KVStore","SET [%s] %s", key, value)
   }
@@ -68,11 +76,12 @@ func (me *KVStore) IsSet(key string) bool {
 }
 
 func (me *KVStore) Get(key string) ([]byte, int16, bool) {
-  value, ok := me.store[key]
+  keymd5 := ch.NewMD5Key(key)
+  ok, valueInt := me.store.Get(keymd5)
   if ok {
-    flags, ok := me.flags[key]
-    me.Logger.Debug("KVStore","GET [%s] %s", key, value)
-    return value, flags, ok
+    value := valueInt.(*Item)
+    me.Logger.Debug("KVStore","GET [%s] %s", value.Key, value.Data)
+    return value.Data, value.Flags, true
   } else {
     me.Logger.Debug("KVStore","GET [%s] NOT FOUND", key)
     return nil, 0, false
@@ -80,9 +89,11 @@ func (me *KVStore) Get(key string) ([]byte, int16, bool) {
 }
 
 func (me *KVStore) Delete(key string) bool {
-  _, ok := me.store[key]
   me.Logger.Debug("KVStore","DELETE [%s]", key)
-  delete(me.store,key)
-  delete(me.flags,key)
-  return ok
+  return me.deleteKey(ch.NewMD5Key(key))
+}
+
+func (me *KVStore) deleteKey(key ch.Key) bool {
+  me.store.Clear(key)
+  return true
 }
--- kvstore/kvstore_test.go
+++ kvstore/kvstore_test.go
@@ -2,11 +2,48 @@ package kvstore
 
 import (
   "testing"
+  "github.com/tomdionysus/trinity/util"
   "github.com/stretchr/testify/assert"
+  "time"
 )
 
 func TestNewKVStore(t *testing.T) { 
   inst := NewKVStore(nil)
 
   assert.NotNil(t, inst)
+}
+
+func TestInit(t *testing.T) { 
+  logger := util.NewLogger("error")
+  inst := NewKVStore(logger)
+
+  inst.Init()
+}
+
+func TestStartStop(t *testing.T) { 
+  logger := util.NewLogger("error")
+  inst := NewKVStore(logger)
+  inst.Start()
+  assert.True(t, inst.running)
+  inst.Stop()
+  time.Sleep(1000*time.Millisecond)
+  assert.False(t, inst.running)
+}
+
+func TestGetSet(t *testing.T) { 
+  logger := util.NewLogger("error")
+  inst := NewKVStore(logger)
+
+  data := []byte{ 0, 1, 2 }
+  inst.Set("one", data, 1, nil)
+
+  val, flag, ok := inst.Get("one")
+  assert.Equal(t, val, data)
+  assert.Equal(t, flag, int16(1))
+  assert.True(t, ok)
+  val, flag, ok = inst.Get("two")
+  assert.Nil(t, val)
+  assert.Equal(t, flag, int16(0))
+  assert.False(t, ok)
+
 }
