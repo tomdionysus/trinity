6e374ca3b008aade226a4858b77d2bb67a0eacd8
Tom Cully <tom.cully@apitalent.co.nz>
2016-01-03 18:05:18 +1300

    Added incoming/outgoing status

--- main_loop_darwin.go
+++ main_loop_darwin.go
@@ -14,8 +14,9 @@ func TrinityMainLoop(svr *network.TLSServer, logger *util.Logger) {
   c := make(chan os.Signal, 1)
   signal.Notify(c, os.Interrupt)
   signal.Notify(c, syscall.SIGTERM)
-  signal.Notify(c, syscall.SIGINFO) // syscall.SIGINFO doesn't exist in linux go.
+  signal.Notify(c, syscall.SIGINFO) // syscall.SIGINFO doesn"t exist in linux go.
 
+  iostatus := map[bool]string{ true: "Incoming", false: "Outgoing" }
   // Wait for SIGINT
   for {
     select {
@@ -25,7 +26,7 @@ func TrinityMainLoop(svr *network.TLSServer, logger *util.Logger) {
             logger.Info("Main", "Status: Node ID %02X", svr.ServerNode.ID)
             logger.Info("Main", "Status: Listener Address %s", svr.Listener.Addr())
             for _, peer := range svr.Connections { 
-              logger.Info("Main", "Status: Peer %02X (%s -> %s), %s", peer.ServerNetworkNode.ID, peer.Connection.LocalAddr(), peer.Connection.RemoteAddr(), network.PeerStateString[peer.State])
+              logger.Info("Main", "Status: Peer %02X (%s %s) %s", peer.ServerNetworkNode.ID, iostatus[peer.Incoming], peer.Connection.RemoteAddr(), network.PeerStateString[peer.State])
             }
           case os.Interrupt:
             fallthrough
