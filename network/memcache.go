27c6db5195735101cb9bdef57ddde9d761b4496a
s18alg <s18alg@protonmail.com>
2019-12-21 23:34:02 +0100

    [protocol][memcached] add `replace` command

--- network/memcache.go
+++ network/memcache.go
@@ -124,6 +124,9 @@ func (mcs *MemcacheServer) handleCommand(addr string, reader *bufio.Reader, writ
 	case "add":
 		mcs.handleAdd(addr, reader, writer, args)
 		return false
+	case "replace":
+		mcs.handleReplace(addr, reader, writer, args)
+		return false
 	case "get":
 		mcs.handleGet(addr, reader, writer, args)
 		return false
@@ -252,6 +255,68 @@ func (mcs *MemcacheServer) handleAdd(addr string, reader *bufio.Reader, writer *
 	}
 
 	if mcs.Server.IsSet(args[1]) == true {
+		writer.WriteString("NOT_STORED\r\n")
+		writer.Flush()
+		return
+	}
+	mcs.Server.SetKey(args[1], buf[:], int16(flags), expparam)
+	writer.WriteString("STORED\r\n")
+	writer.Flush()
+}
+
+func (mcs *MemcacheServer) handleReplace(addr string, reader *bufio.Reader, writer *bufio.Writer, args []string) {
+	if len(args) > 6 || len(args) < 5 {
+		writer.WriteString("ERROR\r\n")
+		writer.Flush()
+		return
+	}
+
+	mcs.Logger.Debug("Memcache", "[%s] -> Set %s", addr, args)
+
+	expirytime, err := strconv.Atoi(args[3])
+	if err != nil {
+		writer.WriteString("SERVER_ERROR\r\n")
+		writer.Flush()
+		return
+	}
+
+	flags, err := strconv.Atoi(args[2])
+	flags = flags & 0xFFFF
+	if err != nil {
+		writer.WriteString("SERVER_ERROR\r\n")
+		writer.Flush()
+		return
+	}
+
+	bytes, err := strconv.Atoi(args[4])
+	if err != nil {
+		writer.WriteString("SERVER_ERROR\r\n")
+		writer.Flush()
+		return
+	}
+
+	var buf []byte = make([]byte, bytes, bytes)
+	n, err := reader.Read(buf)
+	if err != nil || n != len(buf) {
+		writer.WriteString("SERVER_ERROR\r\n")
+		writer.Flush()
+		return
+	}
+
+	_, err = reader.ReadString('\n')
+	if err != nil {
+		writer.WriteString("SERVER_ERROR\r\n")
+		writer.Flush()
+		return
+	}
+
+	var expparam *time.Time = nil
+	if expirytime != 0 {
+		expiry := time.Now().UTC().Add(time.Duration(expirytime) * time.Second)
+		expparam = &expiry
+	}
+
+	if mcs.Server.IsSet(args[1]) == false {
 		writer.WriteString("NOT_STORED\r\n")
 		writer.Flush()
 		return
